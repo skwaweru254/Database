b84f2c2ca10ca74730c0b5c3a8ede3f0fa0037c9
skwaweru254 <skwaweru254@gmail.com>
2023-04-07 23:46:12 +0300

    Commit

--- display.php
+++ display.php
@@ -1,14 +1,43 @@
 <?php
 include 'connect.php';
-if(isset($_POST['submit'])){
+
+if (isset($_POST['submit'])) {
+
     $email = $_POST['email'];
-    $password = $_POST['password'];
-    $image = $_POST['file'];
+    $password = md5($_POST['password']);
+    //$password = crypt($_POST['password']);
+    //$password = password_hash($_POST['password'], PASSWORD_DEFAULT)
+    $image = $_FILES['file'];
 
-    echo $email;
-    echo $password;
-    echo $image;
 
+    $imagefilename = $image['name'];
+    $imagefileerror = $image['error'];
+    $imagefiletemp = $image['tmp_name'];
+    $imagefiletype = $image['type'];
+    $filename_separate = explode('.', $imagefilename);
+    //$file_extension = strtolower($filename_separate[1]);
+    $file_extension = strtolower(end($filename_separate));
+
+
+
+    $extension = array('jpg', 'jpeg', 'png');
+
+    if (in_array($file_extension, $extension)) {
+
+        $upload_image = 'images/' . $imagefilename;
+        move_uploaded_file($imagefiletemp, $upload_image);
+
+        $sql = "insert into `tCRUD` (email,password, profile) values ('$email','$password', '$upload_image')";
+        //$result = mysqli_query($con, $sql);
+
+        if (mysqli_query($con, $sql)) {
+            echo '<script>alert("Data inserted successfully")</script>';
+        } else {
+            die(mysqli_error($con));
+        }
+
+        # code...
+    }
 }
 ?>
 
@@ -32,7 +61,52 @@ if(isset($_POST['submit'])){
 
 
     <!-- YOUR CONTENT STARTS HERE -->
-        <h1 class="text-center my-3">User Details</h1>
+    <h1 class="text-center my-3">User Details</h1>
+    <div class="container mt-5 d-flex justify-content-center">
+
+        <table class="table table-bordered w-50">
+            <thead class="table-dark">
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Password</th>
+                    <th scope="col">Profile</th>
+                </tr>
+            </thead>
+            <tbody>
+
+
+                <?php
+
+                $sql = "select * from `tCRUD`";
+                $result = mysqli_query($con, $sql);
+                while ($row = mysqli_fetch_assoc($result)) {
+
+                    $id = $row['id'];
+                    $email = $row['email'];
+                    $password = $row['password'];
+                    $image1 = $row['profile'];
+
+
+                    echo '<tr>
+                    <td>' . $id . '</td>
+                    <td>' . $email . '</td>
+                    <td>' . $password . '</td>
+                    <td><img src=' . $image1 . ' /></td>
+                </tr>';
+
+                    # code...
+                }
+
+
+
+
+
+                ?>
+
+            </tbody>
+        </table>
+    </div>
 
 
     <!-- YOUR CONTENT ENDS HERE -->
