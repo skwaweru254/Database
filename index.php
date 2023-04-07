262b71fb3934a9ecd22ca05a0e5fcee805e5fadc
skwaweru254 <skwaweru254@gmail.com>
2023-04-07 20:59:39 +0300

    Commit

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ if (isset($_POST['submit'])) {
 
     if ($result) {
         //echo "Data inserted successfully";
-        echo '<script>alert("Data inserted successfully")</script>';
+        echo '<script>alert("Data inserted successfully")window.location.href="display.php";</script>';
     } else {
         die(mysqli_error($con));
     }
@@ -46,20 +46,19 @@ if (isset($_POST['submit'])) {
         <h1 class="text-center my-3">Registration Form</h1>
 
     <div class="container my-5 d-flex justify-content-center">
-        <form method="post" class="w-50">
+        <form method="post" class="w-50" action="display.php">
             <div class="mb-3">
                 <label class="form-label">Email address</label>
-                <input type="email" class="form-control" name="email" required="">
+                <input type="email" class="form-control" name="email" required="" placeholder="Email Address">
             </div>
             <div class="mb-3">
                 <label class="form-label">Password</label>
-                <input type="password" class="form-control" name="password" required="">
+                <input type="password" class="form-control" name="password" required="" placeholder="Password">
             </div>
             <div class="mb-3">
                 <label class="form-label">File</label>
                 <input type="file" class="form-control" name="file" required="">
             </div>
-
             <button type="submit" class="btn btn-primary" name="submit">Submit</button>
         </form>
 
@@ -67,16 +66,6 @@ if (isset($_POST['submit'])) {
 
     <!-- YOUR CONTENT ENDS HERE -->
 
-
-    <!-- JavaScript: placed at the end of the document so the pages load faster -->
-    <!-- jQuery library -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-
-    <!-- Popper -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-
-    <!-- Latest compiled and minified Bootstrap JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 
 </html>
